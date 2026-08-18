a9db01257a9074f865b064615adfbbed5ab15bba
Justinmean <yi936740@gmail.com>
2026-08-18 12:10:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
 -- main.lua
 print("🟢 Loading Hat Orbit Modules...")
 
-local Core = loadstring(game:HttpGet("https://raw.githubusercontent.com/yeming360/hat-orbit/main/core.lua"))()
+local Core = loadstring(game:HttpGet("https://raw.githubusercontent.com/yeming360/hat-orbit/main/core.Init"))()
 print("✅ Core loaded")
 
-local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/yeming360/hat-orbit/main/gui.lua"))()
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/yeming360/hat-orbit/main/gui.Init"))()
 print("✅ GUI loaded")
 
 Core.Init(GUI)
